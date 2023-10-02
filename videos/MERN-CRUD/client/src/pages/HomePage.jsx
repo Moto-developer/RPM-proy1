67e8848b1bf41d6f03f3b1652819455b667de029
Luisito <martinesj405@gmail.com>
2023-10-02 14:38:37 -0500

    update video

--- videos/MERN-CRUD/client/src/pages/HomePage.jsx
+++ videos/MERN-CRUD/client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <img src="https://cdn.pixabay.com/photo/2017/08/21/08/21/directory-2664470_1280.jpg" width='100%'/></div>
+      <img src="https://media.gettyimages.com/id/955410340/es/foto/lionel-messi-of-barcelona-celebrates-after-scoring-his-sides-second-goal-during-the-la-liga.jpg?s=612x612&w=0&k=20&c=j9O_N37ryzUIyu-XTak4JUjd5Z7A6fnuPkO_E0wKI-8= " width='100%'/></div>
   )
 }
 
